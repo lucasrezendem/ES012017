0b789c869342c41700dfde9ac46dacb9ab9102d1
Thiago Urias <thiagourias_@hotmail.com>
2017-04-12 23:46:53 -0300

    Criacao: Home, template, etc

--- Agenda_Cultural/urls.py
+++ Agenda_Cultural/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^$', 'Agenda_Cultural.core.views.home', name='home'),
     url(r'^admin/', admin.site.urls),
 ]
