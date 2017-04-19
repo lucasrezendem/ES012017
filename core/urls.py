3ecd99784cafdf8d2fe0df72851ed538f7a90c92
thiagourias <thiagourias_@hotmail.com>
2017-04-19 12:38:45 -0300

    Criação paginas e referenciação

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^pagina_bares/$', views.bares, name='bares'),
+    url(r'^pagina_esportes/$', views.esportes, name='esportes'),
+	url(r'^pagina_festas/$', views.festas, name='festas'),
+	url(r'^pagina_sobre/$', views.sobre, name='sobre'),
+	url(r'^pagina_teatro/$', views.teatro, name='teatro'),
+	url(r'^pagina_login/$', views.login, name='login'),
     url(r'^home/$', views.home, name='home'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return render(request,  'index.html')
 
-
 def home(request):
-    return render(request,  'home.html',  {'usuario': 'Fulano de tal'})
+    return render(request,  'home.html')
+
+def bares(request):
+    return render(request,  'pagina_bares.html')
+
+def esportes(request):
+    return render(request,  'pagina_esportes.html')
+
+def festas(request):
+    return render(request,  'pagina_festas.html')
+
+def login(request):
+    return render(request,  'pagina_login.html')
+
+def sobre(request):
+    return render(request,  'pagina_sobre.html')
+
+def teatro(request):
+    return render(request,  'pagina_teatro.html')
+
