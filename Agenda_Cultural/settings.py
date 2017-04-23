34397b0c533c4529524c506b3b0be4b1721161fc
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2017-04-22 23:35:35 -0300

    Model Usuarios

--- Agenda_Cultural/settings.py
+++ Agenda_Cultural/settings.py
@@ -87,6 +87,19 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.models.social_update_pipeline',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -160,3 +173,9 @@ SOCIAL_AUTH_FACEBOOK_KEY = '405177329880911'
 SOCIAL_AUTH_FACEBOOK_SECRET = 'c632e32e05410b03136dd72f3abc12a7'
 SOCIAL_AUTH_GITHUB_KEY = '750ab5bf73fd073e08d3'
 SOCIAL_AUTH_GITHUB_SECRET = '3f57267814638cd3b127c876a1632e197e3d45e5'
+
+# Informacoes recebidas pela autenticacao por redes sociais
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = { 'fields': 'id, age_range, name, email, picture' }
+
+AUTH_USER_MODEL = 'account.Usuario'
--- account/models.py
+++ account/models.py
@@ -1,5 +1,89 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import validate_comma_separated_integer_list
+from django.utils.translation import gettext as _
+from django.core.exceptions import ValidationError
+from datetime import date
 
-# Create your models here.
+valid_authenticators = ['local', 'facebook']
+
+
+def validate_authenticator(authenticator):
+    if authenticator not in valid_authenticators:
+        raise ValidationError(
+            _('Invalid authenticator %(authenticator)'),
+            params={'authenticator', authenticator},
+        )
+
+
+def validate_age_range(age_range_string):
+    age_range_list = age_range_string.split(",")
+    if len(age_range_list) != 2:
+        raise ValidationError(_('Invalid list size'))
+    if  (age_range_list[0] < 0) or (age_range_list[1] < 0) or (age_range_list[0] < age_range_list[1]):
+        raise ValidationError(_('Invalid age range'))
+
+
+def validate_birth_date(birthday_date):
+    if birthday_date > date.today():
+        raise ValidationError(_('Invalid birthday date'))
+
+
+class Usuario(AbstractUser):
+    authenticator = models.CharField(max_length = 25, validators=[validate_authenticator])
+    # A autenticacao pelo facebook nao fornece a data de nascimento, apenas
+    # um range da idade.
+    age_range = models.CharField(max_length = 7,
+        validators=[validate_comma_separated_integer_list,validate_age_range],
+        null = True,
+        blank = True,
+    )
+    birthday_date = models.DateField(validators=[validate_birth_date], null = True, blank = True)
+
+    def save_clean(self, *args, **kwargs):
+        self.full_clean()
+        self.save()
+
+    def update_social_info_facebook(self, response):
+        self.authenticator = 'facebook'
+        age_range = response.get('age_range')
+        self.age_range = '%d,%d' % (age_range.get('max'), age_range.get('min'))
+        self.save_clean()
+
+    def initialize_social_info_facebook(self, response):
+        self.birthday_date = None
+        self.update_social_info_facebook(response)
+
+    def update_user_info(self):
+        pass
+
+    def age(self):
+        if self.authenticator == 'local':
+            # Data de nascimento salva
+            born = self.birthday_date
+            if born == None:
+                return None
+            today = date.today()
+            return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+        if self.authenticator == 'facebook':
+            # Idade aproximada pela media do range
+            age_range = [int(x) for x in self.age_range.split(",")]
+            return (age_range[0] + age_range[1]) / 2
+
+
+
+
+def social_update_pipeline(is_new, backend, user, response, *args, **kwargs):
+    if backend.name == 'facebook':
+        if is_new:
+            try:
+                user.initialize_social_info_facebook(response)
+            except:
+                user.delete()
+                raise # Quebra o pipeline da autenticacao
+        else:
+            user.update_social_info_facebook(response)
+    else:
+        raise "Invalid backend name", backend.name
