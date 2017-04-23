3a2a665f8b45f388e93acfe1dd64926669117c7f
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2017-04-23 02:10:41 -0300

    Corrigindo links estaticos

--- account/views.py
+++ account/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-
-@login_required
+# Profile e' o root de account
 def profile(request):
-    return render(request, 'account/profile.html')
+    if request.user.is_authenticated():
+        return render(request, 'account/profile.html')
+    else:
+        return render(request, 'account/login_or_signup.html')
+
+
+def login(request):
+    return render(request, 'account/login.html')
+
+def signup(request):
+    return render(request, 'account/signup.html')
