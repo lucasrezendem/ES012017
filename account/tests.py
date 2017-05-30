d95c544c1f8d388186ae7b861f5bcaa5e16f269d
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2017-05-29 21:29:45 -0300

    Melhoria na cobertura e documentação dos testes.

--- account/tests.py
+++ account/tests.py
@@ -3,35 +3,100 @@ from django.core.urlresolvers import reverse
 from django.conf import settings
 from .models import Usuario, social_update_pipeline
 from datetime import date
+from .forms import SignUpForm, UpdateForm
 
-class ViewsTest(TestCase):
+
+class ViewProfileTest(TestCase):
+    """ Testes unitarios da view profile. """
+
     def setUp(self):
+        """ Insere um usuario no banco de dados para os testes. """
+        Usuario.objects.create_user(
+            username = 'usuario',
+            password = '123456',
+            authenticator = 'local',
+        )
+        Usuario.objects.create_user(
+            username = 'usuario_para_atualizar',
+            password = '123456',
+            authenticator = 'local',
+        )
+
+    def test_user_not_authenticated(self):
+        """ Testa se a resposta para um usuario nao autenticado
+            e' a pagina de cadastro ou login. """
+        self.client.logout()
+        response = self.client.get(reverse('profile'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/login_or_signup.html')
+
+    def test_user_authenticated(self):
+        """ Testa se a resposta para um usuario autenticado e'
+            a pagina de perfil com o formulario de atualizacao. """
+        self.client.logout()
+        self.client.login(username='usuario', password='123456')
+        response = self.client.get(reverse('profile'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/profile.html')
+        self.assertEqual(type(response.context['form']), UpdateForm)
+
+    def test_profile_update(self):
+        """ Testa a atualizacao das informcoes pela pagina profile. """
+        update_user_info = {
+                            'user_type': 'US',
+                            'first_name': 'usuario',
+                            'last_name': 'atualizado',
+                            'birth_date': '1996-10-01',
+                            'email': 'usuario_atualizado@gmail.com',
+                            'update': 'update'
+                        }
+        self.client.logout()
+        self.client.login(username='usuario_para_atualizar', password='123456')
+        response = self.client.post(reverse('profile'), update_user_info)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/profile.html')
+        self.assertEqual(response.context['update_ok'], True)
+
+        # Confere se os dados foram atualizados corretamente
+        self.assertEqual('usuario_para_atualizar', response.context['user'].username)
+        self.assertEqual(update_user_info['user_type'], response.context['user'].user_type)
+        self.assertEqual(update_user_info['first_name'], response.context['user'].first_name)
+        self.assertEqual(update_user_info['last_name'], response.context['user'].last_name)
+        self.assertEqual(update_user_info['email'], response.context['user'].email)
+
+    def test_user_authenticated(self):
+        """ Testa a pagina de cadastro para usuario ja autenticado, o usuario deve ser redirecionado para a view profile. """
+        self.client.logout()
+        self.client.login(username='usuario', password='123456')
+        response = self.client.get(reverse('signup'), follow = True)
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse('profile'))
+        self.assertTemplateUsed(response, 'account/profile.html')
+        self.assertEqual(type(response.context['form']), UpdateForm)
+
+
+
+
+
+class ViewLoginTest(TestCase):
+    """ Testes unitarios da view login_user. """
+
+    def setUp(self):
+        """ Insere um usuario no banco de dados para os testes. """
         Usuario.objects.create_user(
             username = 'usuario',
             password = '123456',
             authenticator = 'local',
         )
 
-    def test_profile(self):
-        # Testa a resposta como usuario nao autenticado
-        response = self.client.get(reverse('profile'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'account/login_or_signup.html')
-
-        # Testa a resposta como usuario autenticado
-        self.client.logout()
-        self.client.login(username='usuario', password='123456')
-        response = self.client.get(reverse('profile'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'account/profile.html')
-
-    def test_login_user(self):
-        # Testa a resposta como usuario nao autenticado
+    def test_user_not_authenticated(self):
+        """ Testa pagina de login para usuarios nao autenticados. """
         response = self.client.get(reverse('login'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'account/login.html')
 
-        # Testa a autenticacao
+    def test_authentication(self):
+        """ Testa tentativa de autenticacao na pagina de login. """
         self.client.logout()
         response = self.client.post(reverse('login'),
                 {'username': 'usuario', 'password': '123456'},
@@ -41,21 +106,59 @@ class ViewsTest(TestCase):
         self.assertRedirects(response, settings.LOGIN_REDIRECT_URL)
         self.assertEqual(response.context['user'].username, 'usuario')
 
-        # Testa a resposta como usuario ja autenticado
+    def test_user_authenticated(self):
+        """ Testa a pagina de login para usuario ja autenticado, o usuario deve ser redirecionado para a view profile. """
         self.client.logout()
         self.client.login(username='usuario', password='123456')
-        response = self.client.get(reverse('profile'))
+        response = self.client.get(reverse('login'), follow = True)
         self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse('profile'))
         self.assertTemplateUsed(response, 'account/profile.html')
+        self.assertEqual(type(response.context['form']), UpdateForm)
 
-    def test_signip(self):
-        # Testa a resposta como usuario nao autenticado
+    def test_login_invalid_user(self):
+        """ Testa a pagina de login no caso de tentativa de login invalido. """
+        self.client.logout()
+        response = self.client.post(reverse('login'),
+                {'username': 'usuario_invalido', 'password': '123456'},
+                follow = True
+            )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/login.html')
+        self.assertEqual(response.context['login_erro'], True)
+
+        self.client.logout()
+        response = self.client.post(reverse('login'),
+                {'username': 'usuario', 'password': 'senha_invalida'},
+                follow = True
+            )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/login.html')
+        self.assertEqual(response.context['login_erro'], True)
+
+
+
+
+class ViewSignupTest(TestCase):
+    """ Testes unitarios da view signup """
+
+    def setUp(self):
+        Usuario.objects.create_user(
+            username = 'usuario',
+            password = '123456',
+            authenticator = 'local',
+        )
+
+    def test_user_not_authenticated(self):
+        """ Testa a pagina de cadastro para um usuario nao autenticado """
+        self.client.logout()
         response = self.client.get(reverse('signup'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'account/signup.html')
-        self.assertIsNotNone(response.context['form'])
+        self.assertEqual(type(response.context['form']), SignUpForm)
 
-        # Testa a criacao de novo usuario
+    def test_user_signup(self):
+        """ Testa o cadastro de novos usuarios pela pagina signup """
         new_user_info = {
                             'username': 'novo_usuario',
                             'user_type': 'PR',
@@ -72,26 +175,34 @@ class ViewsTest(TestCase):
         self.assertRedirects(response, settings.LOGIN_REDIRECT_URL)
         self.assertEqual(response.context['user'].username, 'novo_usuario')
 
-        # Testa se os dados foram salvos corretamente
+        # Tenta logar novamente com o novo usuario para verificar se os dados foram salvos corretamente
         self.client.logout()
-        self.client.login(username='novo_usuario', password='teste123456')
-        response = self.client.get(reverse('index')) # Requisita uma pagina qualquer para recuperar as informacoes do usuario
+        response = self.client.post(reverse('login'),
+                {'username': 'novo_usuario', 'password': 'teste123456'},
+                follow = True
+            )
         self.assertEqual(new_user_info['username'], response.context['user'].username)
         self.assertEqual(new_user_info['user_type'], response.context['user'].user_type)
         self.assertEqual(new_user_info['first_name'], response.context['user'].first_name)
         self.assertEqual(new_user_info['last_name'], response.context['user'].last_name)
         self.assertEqual(new_user_info['email'], response.context['user'].email)
 
-        # Testa a resposta como usuario ja autenticado
+    def test_user_authenticated(self):
+        """ Testa a pagina de cadastro para usuario ja autenticado, o usuario deve ser redirecionado para a view profile. """
         self.client.logout()
         self.client.login(username='usuario', password='123456')
-        response = self.client.get(reverse('profile'))
+        response = self.client.get(reverse('signup'), follow = True)
         self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse('profile'))
         self.assertTemplateUsed(response, 'account/profile.html')
-        pass
+        self.assertEqual(type(response.context['form']), UpdateForm)
 
 
-class UsuarioTestCase(TestCase):
+
+
+class ModelUsuarioCase(TestCase):
+    """ Testes unitarios para o model Usuario. """
+
     def setUp(self):
         Usuario.objects.create(
             username = 'local_ok',
--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.profile, name='profile'),
-    url(r'^login/$', views.login_user, name='login'),
+    url(r'^login/$', views.login, name='login'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
