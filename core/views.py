c3ff2799522569d413110fc7130ba0a038cd8d04
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2017-06-19 13:17:11 -0300

    Removendo arquivos nao utilizados.

--- core/views.py
+++ core/views.py
@@ -4,11 +4,5 @@ from django.http import HttpResponse
 def index(request):
     return render(request,  'index.html')
 
-def home(request):
-    return render(request,  'home.html')
-
-def sobre(request):
-    return render(request,  'pagina_sobre.html')
-
 def about(request):
     return render(request,  'about.html')
