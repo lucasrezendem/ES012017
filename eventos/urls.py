330c7c06e4341c83a2eea3c749fee8950231237e
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2017-06-19 10:57:34 -0300

    Corrigindo pagina teatros.

--- eventos/urls.py
+++ eventos/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     url(r'^cadastro/(?P<tipo_evento>.+)/$', views.cadastro_evento, name='cadastro_evento'),
     url(r'^delete/(?P<tipo_evento>.+)/(?P<nome>.+)/$', views.deleta_evento, name='deleta_evento'),
     url(r'^atualiza/(?P<tipo_evento>.+)/(?P<nome>.+)/$', views.atualiza_evento, name='atualiza_evento'),
-    url(r'^bares/$', views.bares, name='bares'),
-    url(r'^festas/$', views.festas, name='festas'),
-    url(r'^teatro/$', views.teatro, name='teatro'),
-    url(r'^esportes/$', views.esportes, name='esportes'),
+    url(r'^bares/$', views.view_bares, name='bares'),
+    url(r'^festas/$', views.view_festas, name='festas'),
+    url(r'^teatro/$', views.view_teatros, name='teatro'),
+    url(r'^esportes/$', views.view_esportes, name='esportes'),
 ]
