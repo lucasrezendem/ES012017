8ac1530b7500fc0b520dd6451eb6af26567dc22a
gabrielpereirapinheiro <140020764@aluno.unb.br>
2017-06-27 17:29:09 -0300

    testes com a ajuda do gustavohfc

--- eventos/tests.py
+++ eventos/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.conf import settings
+from .models import *
+from datetime import date
 
 # Create your tests here.
+
+class view_bar(TestCase):
+    def teste_template(self):
+        response = self.client.get(reverse('bares'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'eventos/pagina_bares.html')
+
+
+class view_esporte(TestCase):
+    def teste_template(self):
+        response = self.client.get(reverse('esportes'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'eventos/pagina_esportes.html')
+
+
